836b54009abea5e501a4c824de4fdd89348b3add
Tanner Cranor <tbudc@yahoo.com>
2018-11-14 17:45:35 +0000

    Task submission working

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-  $("#postComment").click(function(){
-      var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+  $("#postTask").click(function(){
+      var myobj = {Task:$("#task").val(),Date:$("#duedate").val(), Complete:false};
+      $("#goTaskForm")[0].reset();
       jobj = JSON.stringify(myobj);
       $("#json").text(jobj);
-      var url = "comment";
+      var url = "task";
       $.ajax({
         url:url,
         type: "POST",
@@ -14,52 +15,79 @@ $(document).ready(function(){
         }
       })
   });
-  $("#getComments").click(function() {
-    $.getJSON('comment', function(data) {
+  $("#getTasks").click(function() {
+    $.getJSON('task', function(data) {
       console.log(data);
       var everything = "<ul>";
-      for(var comment in data) {
-        com = data[comment];
-        everything += "<li>" + com.Name + " says: " + com.Comment + "</li>";
+      for(var task in data) {
+        task = data[task];
+        everything += "<li>" + task.Task + " due on: " + task.Date + "</li>";
       }
       everything += "</ul>";
       $("#comments").html(everything);
     })
   });
-  $("#deleteComments").click(function() {
-    var url = "delete";
-    console.log("handler");
-      $.ajax({
-        url:url,
-        type: "POST",
-        success: function(data,textStatus) {
-            $("#done").html(textStatus);
-        }
-      })
-  });
-  $("#getUserComments").click(function(){
-      var myobj = {Name:$("#searchname").val()};
-      console.log(myobj);
-      jobj = JSON.stringify(myobj);
-      console.log(jobj);
-      $("#json").text(jobj);
-      var url = "user";
-      $.ajax({
-        url:url,
-        type: "POST",
-        data: jobj,
-        contentType: "application/json; charset=utf-8",
-        success: function(data,textStatus) {
-          $("#done").html(textStatus);
-          console.log(data);
-          var everything = "<ul>";
-          for(var comment in data) {
-            com = data[comment];
-            everything += "<li>" + com.Name + " says: " + com.Comment + "</li>";
-          }
-          everything += "</ul>";
-          $("#comments").html(everything);
-        }
-      })
-  });
+//   $("#postComment").click(function(){
+//       var myobj = {Name:$("#name").val(),Comment:$("#comment").val()};
+//       jobj = JSON.stringify(myobj);
+//       $("#json").text(jobj);
+//       var url = "comment";
+//       $.ajax({
+//         url:url,
+//         type: "POST",
+//         data: jobj,
+//         contentType: "application/json; charset=utf-8",
+//         success: function(data,textStatus) {
+//             $("#done").html(textStatus);
+//         }
+//       })
+//   });
+//   $("#getComments").click(function() {
+//     $.getJSON('comment', function(data) {
+//       console.log(data);
+//       var everything = "<ul>";
+//       for(var comment in data) {
+//         com = data[comment];
+//         everything += "<li>" + com.Name + " says: " + com.Comment + "</li>";
+//       }
+//       everything += "</ul>";
+//       $("#comments").html(everything);
+//     })
+//   });
+//   $("#deleteComments").click(function() {
+//     var url = "delete";
+//     console.log("handler");
+//       $.ajax({
+//         url:url,
+//         type: "POST",
+//         success: function(data,textStatus) {
+//             $("#done").html(textStatus);
+//         }
+//       })
+//   });
+//   $("#getUserComments").click(function(){
+//       var myobj = {Name:$("#searchname").val()};
+//       console.log(myobj);
+//       jobj = JSON.stringify(myobj);
+//       console.log(jobj);
+//       $("#json").text(jobj);
+//       var url = "user";
+//       $.ajax({
+//         url:url,
+//         type: "POST",
+//         data: jobj,
+//         contentType: "application/json; charset=utf-8",
+//         success: function(data,textStatus) {
+//           $("#done").html(textStatus);
+//           console.log(data);
+//           var everything = "<ul>";
+//           for(var comment in data) {
+//             com = data[comment];
+//             everything += "<li>" + com.Name + " says: " + com.Comment + "</li>";
+//           }
+//           everything += "</ul>";
+//           $("#comments").html(everything);
+//         }
+//       })
+//   });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,15 @@ var router = express.Router();
 /* Set up mongoose in order to connect to mongo database */
 var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
 
-mongoose.connect('mongodb://localhost/commentDB', { useMongoClient: true }); //Connects to a mongo database called "commentDB"
+mongoose.connect('mongodb://localhost/taskDB', { useMongoClient: true }); //Connects to a mongo database called "commentDB"
 
-var commentSchema = mongoose.Schema({ //Defines the Schema for this database
-Name: String,
-Comment: String
+var taskSchema = mongoose.Schema({ //Defines the Schema for this database
+Task: String,
+Date: Date,
+Complete: Boolean
 });
 
-var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
+var Task = mongoose.model('Task', taskSchema); //Makes an object from that schema as a model
 
 var db = mongoose.connection; //Saves the connection as a variable to use
 db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
@@ -20,54 +21,54 @@ console.log('Connected');
 });
 
 /* GET comments from database */
-router.get('/comment', function(req, res, next) {
+router.get('/task', function(req, res, next) {
 console.log("In the GET route?");
-Comment.find(function(err,commentList) { //Calls the find() method on your database
+Task.find(function(err,taskList) { //Calls the find() method on your database
   if (err) return console.error(err); //If there's an error, print it out
   else {
-    console.log(commentList); //Otherwise console log the comments you found
-    res.json(commentList); //Then send the comments
+    console.log(taskList); //Otherwise console log the tasks you found
+    res.json(taskList); //Then send the tasks
     
   }
 })
 });
 
-/* GET user comments from database */
-router.post('/user', function(req, res, next) {
-console.log("In the USER route?");
-console.log(req.body);
-Comment.find(req.body, function(err,commentList) { //Calls the find() method on your database
-  if (err) return console.error(err); //If there's an error, print it out
-  else {
-    console.log(commentList); //Otherwise console log the comments you found
-    res.json(commentList); //Then send the comments
+// /* GET user comments from database */
+// router.post('/user', function(req, res, next) {
+// console.log("In the USER route?");
+// console.log(req.body);
+// Comment.find(req.body, function(err,commentList) { //Calls the find() method on your database
+//   if (err) return console.error(err); //If there's an error, print it out
+//   else {
+//     console.log(commentList); //Otherwise console log the comments you found
+//     res.json(commentList); //Then send the comments
     
-  }
-})
-});
+//   }
+// })
+// });
 
-router.post('/comment', function(req, res, next) {
-    console.log("POST comment route"); //[1]
+router.post('/task', function(req, res, next) {
+    console.log("POST task route"); //[1]
     console.log(req.body); //[2]
-    var newcomment = new Comment(req.body); //[3]
-    console.log(newcomment); //[3]
-    newcomment.save(function(err, post) { //[4]
+    var newtask = new Task(req.body); //[3]
+    console.log(newtask); //[3]
+    newtask.save(function(err, post) { //[4]
       if (err) return console.error(err);
       console.log(post);
       res.sendStatus(200);
     });
 });
 
-/* REMOVE comments from database */
-router.post('/delete', function(req, res, next) {
-console.log("In the REMOVE route?");
-Comment.deleteMany({}, function(err,result) {
-  if (err) return console.error(err); //If there's an error, print it out
-  else {
-    console.log(result); //Otherwise console log the comments you found
-    res.sendStatus(200);
-  }
-})
-})
+// /* REMOVE comments from database */
+// router.post('/delete', function(req, res, next) {
+// console.log("In the REMOVE route?");
+// Comment.deleteMany({}, function(err,result) {
+//   if (err) return console.error(err); //If there's an error, print it out
+//   else {
+//     console.log(result); //Otherwise console log the comments you found
+//     res.sendStatus(200);
+//   }
+// })
+// })
 
 module.exports = router;
